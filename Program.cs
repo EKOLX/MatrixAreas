94e3469d90e1f4ec7bc3d29b528b6cdb1f568e86
EL Khan <elkhanmursali@gmail.com>
2023-09-15 10:45:39 +0400

    Changed the logic. Misunderstood 1st time.

--- Program.cs
+++ Program.cs
@@ -2,67 +2,70 @@
 
 Console.Write($"{Environment.NewLine}Enter a matrix of values:  ");
 var values = Console.ReadLine();
+Console.WriteLine();
 
 if(string.IsNullOrEmpty(values))
 {
-    Console.WriteLine($"{Environment.NewLine}No values!");
+    Console.WriteLine($"No values!{Environment.NewLine}");
 }
 else
 {
-    int zoneCount = 0;
     var rows = values.Split(';');
-    Console.WriteLine();
+    
+    int[][] matrix = new int[rows.Length][];
 
     for(int rowIndex = 0; rowIndex < rows.Length; rowIndex++)
     {
         var columns = rows[rowIndex].Split(',');
-        Console.WriteLine(rows[rowIndex]);
-        
-        for(int columnIndex = 0; columnIndex < columns.Length; columnIndex++)
-        {
-            var currentValue = int.Parse(columns[columnIndex]);
-            // if value of cell is 0 then move on
-            if(currentValue == 0) continue;
-            // if value of cell is 1 then check its all 4 sides
-            else
-            {
-                // CHECK LEFT SIDE
-                if(columnIndex > 0)
-                {
-                    var leftValue = int.Parse(columns[columnIndex - 1]);
-                    if(leftValue == 1) continue;
-                }
-
-                // CHECK RIGHT SIDE
-                if(columnIndex + 1 < columns.Length)
-                {
-                    var rightValue = int.Parse(columns[columnIndex + 1]);
-                    if(rightValue == 1) continue;;
-                }
-
-                //  CHECK TOP SIDE
-                if(rowIndex > 0)
-                {
-                    var topValue = int.Parse(rows[rowIndex - 1].Split(',')[columnIndex]);
-                    if(topValue == 1) continue;
-                }
-
-                // CHECK BOTTOM SIDE
-                if(rowIndex + 1 < rows.Length)
-                {
-                    var bottomValue = int.Parse(rows[rowIndex + 1].Split(',')[columnIndex]);
-                    if(bottomValue == 1) continue;
-                }
-
-                zoneCount++;
-            } 
-        }
+        matrix[rowIndex] = columns.Select(int.Parse).ToArray();
     }
+    
+    int zoneOneCount = new MatrixAreas().FindZonesOne(matrix);
 
-    Console.WriteLine($"{Environment.NewLine}Number of areas: {zoneCount}{Environment.NewLine}");
+    Console.WriteLine($"{Environment.NewLine}Number of areas: {zoneOneCount}{Environment.NewLine}");
 }
 
 Console.WriteLine("------ Matrix Areas ------");
 
 Console.Write($"{Environment.NewLine}Press any key to exit...");
 Console.ReadKey(true);
+
+class MatrixAreas
+{
+    public int FindZonesOne(int[][] matrix)
+    {
+        int zoneOneCount = 0;
+
+        for(int rowIndex = 0; rowIndex < matrix.Length; rowIndex++)
+        {
+            for(int columnIndex = 0; columnIndex < matrix[rowIndex].Length; columnIndex++)
+            {
+                if(matrix[rowIndex][columnIndex] == 1)
+                {
+                    CheckArea(matrix, rowIndex, columnIndex);
+                    zoneOneCount++;
+                }
+            }
+        }
+
+        return zoneOneCount;
+    }
+
+    private void CheckArea(int[][] matrix, int rowIndex, int columnIndex)
+    {
+        // Check the boundaries and whether cell is 1
+        if(rowIndex < 0 || rowIndex >= matrix.Length
+           || columnIndex < 0 || columnIndex >= matrix[rowIndex].Length
+           || matrix[rowIndex][columnIndex] != 1)
+           return;
+
+        // Mark current cell as visited by value 2
+        matrix[rowIndex][columnIndex] = 2;
+
+        // Check all 4 directions
+        CheckArea(matrix, rowIndex, columnIndex - 1); // LEFT
+        CheckArea(matrix, rowIndex, columnIndex + 1); // RIGHT
+        CheckArea(matrix, rowIndex - 1, columnIndex); // UP
+        CheckArea(matrix, rowIndex + 1, columnIndex); // DOWN
+    }
+}
